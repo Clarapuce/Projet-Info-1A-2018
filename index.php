c51891a6234dbd3a4c3be35f748cc22ff3af9de8
Clara Sorita <claraso@hotmail.fr>
2018-02-27 13:06:20 +0100

    Create index.php
    - Inclue le header
    -Contient l'affichage des session

---
+++ index.php
@@ -0,0 +1,54 @@
+
+    <?php
+        include("header.php");
+        
+        require("connect.php");
+        $MaRequete="SELECT * FROM SESSIONS Order By nom_session";
+        $MonRs = $BDD -> query( $MaRequete );
+        $retour = 1;
+        echo '<div class="container">
+        <table><tr>';
+            while ($Tuple = $MonRs ->fetch() )
+            {
+                echo '
+                <td>
+                <div class="card text-white bg-secondary mb-4" style="max-width: 24rem;">
+                    <div class="card-header">
+                    <a href="index.php?id='.$Tuple["nom_session"].'">
+                        <h3>'.$Tuple["nom_session"].
+                        '</h3>
+                    </a>
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            <p>Créée par '.$Tuple["auteur"].'
+                            </p>
+                        </h5>
+                        <p>'.$Tuple["description"].'</p>
+                    </div>
+                </div>
+              <article>
+                    
+                    
+                </article>
+                </td>';
+                // Changer couleur lien dans css
+                // Changer taille du texte
+                //Changer espacement
+                if($retour==2)
+                {
+                    $retour=1;
+                    echo'</tr><tr>';
+                }
+                else{$retour ++;}
+                
+            }
+        echo '</table></div>';   
+        
+        
+
+
+        //include("footer.php");
+    ?>
+    </body>
+</html>
