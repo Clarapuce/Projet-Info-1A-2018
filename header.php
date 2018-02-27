3945844d7bf4dfc5a48e26b0afda11eea819f046
Clara Sorita <claraso@hotmail.fr>
2018-02-27 13:08:54 +0100

    Create header.php

---
+++ header.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<?php session_start(); ?>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <title>Projet Web</title>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        
+        <title>Projet Web <?php if (isset($pageTitle)) echo "- $pageTitle"; ?></title>
+        <!-- jQuesry -->
+        <script src="jquery/jquery.min.js"></script>
+        <!-- JavaScript Boostrap plugin -->
+        <script src="js/bootstrap.min.js"></script>
+    </head>
+        <body>
+            
+
+               
+        
