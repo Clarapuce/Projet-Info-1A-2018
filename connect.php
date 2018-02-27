1aaff3d37e5a7226241023075a076ee5f9d52781
Clara Sorita <claraso@hotmail.fr>
2018-02-27 13:09:12 +0100

    Create connect.php

---
+++ connect.php
@@ -0,0 +1,9 @@
+<?php
+    $BDD=new PDO(
+        "mysql:hst=localhost;dbname=projet web;charset=utf8",
+        "administrateur",
+        "admin",
+        array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+        //$CurseurPays=$bdd -> query($MaRequete);
+        //$tuple=$CurseurPays->fetch();
+?>
