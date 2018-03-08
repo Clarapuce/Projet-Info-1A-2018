806836d7c86d23883c49a8265c520fbe3613890d
MayRandom <36887259+MayRandom@users.noreply.github.com>
2018-03-08 15:10:57 +0100

    Add files via upload

---
+++ connexion_validee.php
@@ -0,0 +1,22 @@
+<?php 
+include('header.php');
+require("connect.php"); 
+
+$pseudo = $_POST['pseudo'];
+$mdp = $_POST['mdp'];
+
+$resultat = $BDD -> query("SELECT statutStaff FROM utilisateurs WHERE pseudo = '".$pseudo."' AND mdp = '".$mdp."'");
+$ligne = $resultat -> fetch();
+
+if ($ligne==null)
+{
+    echo '<body onLoad="alert(\'Membre non reconnu\')">';
+    echo '<meta http-equiv="refresh" content="0;URL=connexion.php">';
+}
+else
+{
+    $_SESSION['statutStaff']= implode("",$ligne);
+    $_SESSION['pseudo'] = $pseudo;
+    header('location: index.php');
+    exit();
+}?>
