4ddd639d4a6988953919a101142b3b51c7b24b89
MayRandom <36887259+MayRandom@users.noreply.github.com>
2018-03-08 15:11:26 +0100

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,9 +1,13 @@
 <?php
+try
+{
     $BDD=new PDO(
         "mysql:hst=localhost;dbname=projet_web;charset=utf8",
         "administrateur",
         "admin",
         array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-        //$CurseurPays=$bdd -> query($MaRequete);
-        //$tuple=$CurseurPays->fetch();
+}
+catch (Exception $e) {
+    die('Erreur fatale : ' . $e->getMessage());
+}  
 ?>
