6ec41d183e58440ea75f9affb35fbd16de052342
MayRandom <36887259+MayRandom@users.noreply.github.com>
2018-03-10 22:21:23 +0100

    Update deconnexion.php

--- deconnexion.php
+++ deconnexion.php
@@ -1,9 +1,14 @@
 <?php
-    session_start ();
+include('header.php');
 
+if (empty($_SESSION['pseudo'])) erreur(ERR_IS_DE);
+
+else
+{
     session_unset ();
 
     session_destroy ();
 
     header ('location: index.php');
+}
 ?>
