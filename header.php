ab0603d24125f34b077fd0d5bd113c86c80b79c7
MayRandom <36887259+MayRandom@users.noreply.github.com>
2018-03-10 22:21:46 +0100

    Update header.php

--- header.php
+++ header.php
@@ -1,5 +1,8 @@
 <!doctype html>
-<?php session_start(); ?>
+<?php session_start(); 
+include('functions.php');
+include('constants.php');     
+?>
 <html>
     <head>
         <meta charset="utf-8"/>
@@ -16,53 +19,28 @@
         <body>
         
         <div class="header">
-        <a href="index.php" class="liens1">Accueil</a>
-    <span class="h_responsive_br">
-        <p id="menu">Menu</p>
-        <div id="dropdown" class="dropdown-content">
-            <span><?php creerSession() ?>
-            <?php connexion() ?></span>
+            <a href="index.php" class="liens1">Accueil</a>
+            <span class="h_responsive_br">
+                <p id="menu">Menu</p>
+                <div id="dropdown" class="dropdown-content">
+                    <span><?php creerSession() ?>
+                    <?php connexion() ?></span>
+                </div>
+            </span>
+            <script>
+            // Get the button, and when the user clicks on it, execute myFunction
+            document.getElementById("menu").onclick = function() {myFunction()};
+
+            /* myFunction toggles between adding and removing the show class, which is used to hide and show the dropdown content */
+            function myFunction() {
+            document.getElementById("dropdown").classList.toggle("show");
+            }
+            </script>
+            <span class="h_responsive">
+                <?php creerSession() ?>
+                <?php connexion() ?>
+            </span>
         </div>
-    </span>
-        <script>
-// Get the button, and when the user clicks on it, execute myFunction
-document.getElementById("menu").onclick = function() {myFunction()};
 
-/* myFunction toggles between adding and removing the show class, which is used to hide and show the dropdown content */
-function myFunction() {
-document.getElementById("dropdown").classList.toggle("show");
-}
-</script>
+       <br /><br /><br />
 
-        
-    <span class="h_responsive">
-        <?php creerSession() ?>
-        <?php connexion() ?>
-    </span>
-    </div>
-       <br /><br />
-       <?php
-       function creerSession()
-       {
-           if (isset($_SESSION['statutStaff']) AND isset($_SESSION['pseudo']) AND ($_SESSION['statutStaff']==11))
-           {
-               echo '<a href="creerSession.php" class="liens2">Créer une session</a>';
-           }
-           else
-           {
-               
-           }
-       }
-       function connexion()
-       {
-           if (isset($_SESSION['pseudo']))
-           {
-               echo '<a href="creerPerso.php" class="liens2">Créer un personnage</a> 
-               <a href="deconnexion.php" title="deconnexion" class="liens3">Se déconnecter</a>';
-           }
-           else
-           {
-               echo '<a href="inscription.php" title="inscription" class="liens3">S\'inscrire</a> <a href="connexion.php" title="connexion" class="liens3">Se connecter</a>';
-           }
-       }
-       ?>
