77bf79d0e209512ba28f332885b7a10098961e45
Clara Sorita <claraso@hotmail.fr>
2018-03-23 15:58:15 +0100

    Add files via upload

--- stat.php
+++ stat.php
@@ -2,5 +2,18 @@
 include("header.php");
 require("connect.php");
 
-include("footer.php");
+$maison = array("Gryffondor","Serdaigle","Poufsouffle","Serpentard");
+for ($i = 0; $i<4;$i++)
+{
+    $MaRequette="SELECT COUNT(maison) 
+                FROM (SELECT * FROM FORMCOURT WHERE maison ='".$maison[$i]."') AS TYPEMAISON;";
+    $requete = $BDD -> query( $MaRequette);
+    $tab=$requete -> fetch();
+    $taux_maison = $tab[0];
+    echo $maison[$i]." : ".$taux_maison;
+
+}
+?>
+
+<?php include("footer.php");
 ?>
